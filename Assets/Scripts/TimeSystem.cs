81b4695a52ace2e9f7ac0eca310cd6d0daf350e9
Grant King <alpharock6@gmail.com>
2020-08-22 13:00:37 -0700

    TimeSystem base script

---
+++ Assets/Scripts/TimeSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSystem : MonoBehaviour
+{
+    public static TimeSystem instance;
+
+    private void Awake() {
+        if (instance) {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
