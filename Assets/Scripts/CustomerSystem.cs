6855cb859472ca2c9e4d0320bd6c4a11ca4e327f
Danielle Schugars <danielle.schugars@gmail.com>
2020-08-23 16:27:41 -0700

    add possible quests for customers

--- Assets/Scripts/CustomerSystem.cs
+++ Assets/Scripts/CustomerSystem.cs
@@ -13,6 +13,9 @@ public class CustomerSystem : MonoBehaviour
 
     private Dictionary<CustomerType, Customer> customerTypeDict = new Dictionary<CustomerType, Customer>();
 
+    private enum QuestType { BASIC_FIGHT, ADVANCED_FIGHT, FOOD }
+    private Dictionary<QuestType, Quest> questTypeDict = new Dictionary<QuestType, Quest>();
+
     private void Awake() {
         if (instance)
         {
@@ -21,14 +24,50 @@ public class CustomerSystem : MonoBehaviour
         }
         instance = this;
 
-        // Initialize customer types
+        initializeQuestTypes();
+        initializeCustomerTypes();   
+    }
+    
+    private void initializeQuestTypes()
+    {
+        string basicFightDialogue = "I'm going out to fight some Level 1 Slimes.";
+        InventorySystem.SellableItem[] basicFightItems = new InventorySystem.SellableItem[] {
+            InventorySystem.SellableItem.WOODEN_SHIELD,
+            InventorySystem.SellableItem.WOODEN_SWORD
+        };
+        Quest basicFightQuest = new Quest(basicFightDialogue, basicFightItems);
+        questTypeDict.Add(QuestType.BASIC_FIGHT, basicFightQuest);
+
+        string advancedFightDialogue = "I'm fighting a dragon today. Wish me luck!";
+        InventorySystem.SellableItem[] advancedFightItems = new InventorySystem.SellableItem[] {
+            InventorySystem.SellableItem.IRON_SHIELD,
+            InventorySystem.SellableItem.IRON_SWORD
+        };
+        Quest advancedFightQuest = new Quest(advancedFightDialogue, advancedFightItems);
+        questTypeDict.Add(QuestType.ADVANCED_FIGHT, advancedFightQuest);
+
+        string foodDialogue = "I'm so hungry. Got anything to eat around here?";
+        InventorySystem.SellableItem[] foodItems = new InventorySystem.SellableItem[] {
+            InventorySystem.SellableItem.BREAD
+        };
+        Quest foodQuest = new Quest(foodDialogue, foodItems);
+        questTypeDict.Add(QuestType.FOOD, foodQuest);
+    }
+
+    private void initializeCustomerTypes()
+    {
         // Novice
         InventorySystem.SellableItem[] noviceDesiredItems = new InventorySystem.SellableItem[] {
             InventorySystem.SellableItem.BREAD,
             InventorySystem.SellableItem.WOODEN_SHIELD,
             InventorySystem.SellableItem.WOODEN_SWORD
         };
-        Customer noviceCustomer = new Customer(noviceSprite, noviceDesiredItems);
+        Quest[] noviceQuests = new Quest[]
+        {
+            questTypeDict[QuestType.BASIC_FIGHT],
+            questTypeDict[QuestType.FOOD]
+        };
+        Customer noviceCustomer = new Customer(noviceSprite, noviceDesiredItems, noviceQuests);
         customerTypeDict.Add(CustomerType.NOVICE, noviceCustomer);
 
         // Warrior
@@ -38,7 +77,12 @@ public class CustomerSystem : MonoBehaviour
             InventorySystem.SellableItem.IRON_SHIELD,
             InventorySystem.SellableItem.IRON_SWORD
         };
-        Customer warriorCustomer = new Customer(warriorSprite, warriorDesiredItems);
+        Quest[] warriorQuests = new Quest[]
+        {
+            questTypeDict[QuestType.ADVANCED_FIGHT],
+            questTypeDict[QuestType.FOOD]
+        };
+        Customer warriorCustomer = new Customer(warriorSprite, warriorDesiredItems, warriorQuests);
         customerTypeDict.Add(CustomerType.WARRIOR, warriorCustomer);
     }
 
@@ -59,10 +103,24 @@ public class Customer
 {
     public Sprite sprite;
     public InventorySystem.SellableItem[] desiredItems;
+    public Quest[] quests;
 
-    public Customer(Sprite sprite, InventorySystem.SellableItem[] desiredItems)
+    public Customer(Sprite sprite, InventorySystem.SellableItem[] desiredItems, Quest[] quests)
     {
         this.sprite = sprite;
         this.desiredItems = desiredItems;
+        this.quests = quests;
+    }
+}
+
+public class Quest
+{
+    public string description;
+    public InventorySystem.SellableItem[] applicableItems;
+
+    public Quest(string description, InventorySystem.SellableItem[] applicableItems)
+    {
+        this.description = description;
+        this.applicableItems = applicableItems;
     }
 }
