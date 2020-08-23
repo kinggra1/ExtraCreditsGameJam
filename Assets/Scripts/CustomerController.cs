e236ea65751c5691fabca9c0b35cc3d866ba2bbb
Danielle Schugars <danielle.schugars@gmail.com>
2020-08-23 15:54:14 -0700

    create different customer types based on random distribution

---
+++ Assets/Scripts/CustomerController.cs
@@ -0,0 +1,67 @@
+﻿using JetBrains.Annotations;
+using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomerController : MonoBehaviour
+{
+    // where to spawn in the next customer
+    public int MinXCustomerPos;
+    public int MaxXCustomerPos;
+    public int MinYCustomerPos;
+    public int MaxYCustomerPos;
+
+    // speed that customer moves towards their standing position
+    public float speed;
+
+    // where customer enters the screen
+    public Vector3 StartPos;
+
+    // place on the screen the customer walks to
+    private Vector3 standingPos;
+
+    private Customer customer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.transform.localPosition = StartPos;
+
+        determineCustomerType();
+        chooseStandingPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool paused = TimeSystem.instance.GetPaused();
+        if (!paused && this.gameObject.transform.localPosition != standingPos)
+        {
+            moveTowardsStandingPos();
+        }
+    }
+
+    void determineCustomerType()
+    {
+        customer = CustomerSystem.instance.GetRandomCustomerFromDistribution();
+        UnityEngine.UI.Image image = this.gameObject.GetComponent<UnityEngine.UI.Image>();
+        image.sprite = customer.sprite;
+    }
+
+    void chooseStandingPosition()
+    {
+        System.Random rand = GameController.instance.Rand;
+        int xPos = rand.Next(MinXCustomerPos, MaxXCustomerPos);
+        int yPos = rand.Next(MinYCustomerPos, MaxYCustomerPos);
+        standingPos = new Vector3(xPos, yPos, 0);
+    }
+
+    void moveTowardsStandingPos()
+    {
+        float step = speed * Time.deltaTime;
+        this.gameObject.transform.localPosition = Vector3.MoveTowards(this.gameObject.transform.localPosition, standingPos, step);
+    }
+}
---
+++ Assets/Scripts/CustomerSystem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerSystem : MonoBehaviour
+{
+    public static CustomerSystem instance;
+
+    public Sprite noviceSprite;
+    public Sprite warriorSprite;
+
+    public enum CustomerType { NOVICE, WARRIOR }
+
+    private Dictionary<CustomerType, Customer> customerTypeDict = new Dictionary<CustomerType, Customer>();
+
+    private void Awake() {
+        if (instance)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+
+        // Initialize customer types
+        Customer noviceCustomer = new Customer(noviceSprite);
+        customerTypeDict.Add(CustomerType.NOVICE, noviceCustomer);
+
+        Customer warriorCustomer = new Customer(warriorSprite);
+        customerTypeDict.Add(CustomerType.WARRIOR, warriorCustomer);
+    }
+
+    public Customer GetRandomCustomerFromDistribution()
+    {
+        System.Random rand = GameController.instance.Rand;
+        int generatedNumber = rand.Next(0, 100);
+        if (generatedNumber > 80)
+        {
+            return customerTypeDict[CustomerType.WARRIOR];
+        }
+        return customerTypeDict[CustomerType.NOVICE];
+    }
+
+}
+
+public class Customer
+{
+    public Sprite sprite;
+
+    public Customer(Sprite sprite)
+    {
+        this.sprite = sprite;
+    }
+}
